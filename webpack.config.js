8207eab3ff958d0994ea033a4a7ee594df820dc9
Nikolai Maasø Lunde <nikolai.maaso.lunde@digdir.no>
2025-01-21 13:12:02 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,46 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack'); // Import dotenv-webpack
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                type: 'asset/resource',
-            },
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
+
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        entry: './src/index.tsx',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'bundle.js',
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        devServer: {
+            port: 3000,
+            open: true,
+            hot: true,
+        },
+        plugins: [
+            ...(isDevelopment ? [new Dotenv()] : []),
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+            }),
         ],
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        hot: true,
-    },
-    plugins: [
-        new Dotenv(), // Add dotenv-webpack here
-        new HtmlWebpackPlugin({
-            template: './public/index.html', // Ensure this matches your HTML file
-        }),
-    ],
+    };
 };
